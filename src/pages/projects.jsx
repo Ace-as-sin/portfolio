108f900d904e5a46278f6b29f327efc54fe96f18
Blake <BlakeTrapnell.dev@gmail.com>
2022-08-31 20:49:38 -0700

    removed something

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,6 @@ export default function Projects() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Projects</h2>
-        <h4>this is a work in progress</h4>
       </main>
     );
   }
--- src/pages/socials.jsx
+++ src/pages/socials.jsx
@@ -2,7 +2,6 @@ export default function Socials() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Socials</h2>
-        <h4>this is a work in progress</h4>
       </main>
     );
   }
