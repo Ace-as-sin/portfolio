0db2ba739a4287da783a52deb812bbb7ddf1e88f
Blake Trapnell <95154248+Ace-as-sin@users.noreply.github.com>
2024-04-11 14:45:32 -0600

    removed unused function and imports'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import Nav from "./components/nav/nav.js";
-import { motion } from "framer-motion";
 import Introduction from "./components/home/Introduction.js";
 import "./App.scss";
 
 export default function App() {
-  const colorHover = (color, colorHover, text, fontSize) => {
-    return (
-      <motion.strong
-        whileHover={{
-          textShadow: `0 0 10px ${colorHover}`,
-          transition: { duration: 0.2 },
-        }}
-        style={{
-          textShadow: `0 0 30px ${colorHover}`,
-          transition: "color 0.5s", // Transition for color change
-          color,
-          fontSize: fontSize || "inherit",
-        }}
-        transition={{ duration: 0.2 }}
-      >
-        {text}
-      </motion.strong>
-    );
-  };
   return (
     <div className="home--wrapper">
       <Nav />
