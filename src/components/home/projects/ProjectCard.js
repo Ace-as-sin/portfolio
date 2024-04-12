5f67659a31bebb13622126eed64b468d2904fdb3
Blake Trapnell <95154248+Ace-as-sin@users.noreply.github.com>
2024-04-12 00:25:37 -0600

    added mobile styles to projects section

--- src/components/home/projects/ProjectCard.js
+++ src/components/home/projects/ProjectCard.js
@@ -1,26 +1,37 @@
 import React from "react";
 import "./projectCard.scss";
 import { motion } from "framer-motion";
+import { useState, useLayoutEffect } from "react";
 
 const ProjectCard = ({ image, skills, link, reverse = false, clipPath }) => {
   console.log(skills);
 
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useLayoutEffect(() => {
+    function updateSize() {
+      setIsSmallScreen(window.innerWidth < 768);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   return (
     <motion.a
-      className="project-card"
-      whileHover={{
-        boxShadow: `0 0 28px ${skills[0].color}`,
-        transition: { duration: 0.2 }, // Duration for hover animation
-      }}
-      href={link}
-      target="_blank norel nonoppener"
-      style={{
-        flexDirection: !reverse ? "row" : "row-reverse",
-        transition: "box-shadow 0.3s", // Transition for box shadow change
-        boxShadow: `0 0 12px ${skills[0].color}`,
-      }}
-    >
-
+    className="project-card"
+    whileHover={{
+      boxShadow: `0 0 36px ${skills[0].color}`,
+      transition: { duration: 0.2 }, // Duration for hover animation
+    }}
+    href={link}
+    target="_blank norel nonoppener"
+    style={{
+      flexDirection: isSmallScreen ? "column" : (reverse ? "row-reverse" : "row"),
+      transition: "box-shadow 0.3s", // Transition for box shadow change
+      boxShadow: `0 0 24px ${skills[0].color}`,
+    }}
+  >
       <img
         className="project--website-photo"
         src={image}
--- src/components/home/projects/Projects.js
+++ src/components/home/projects/Projects.js
@@ -11,7 +11,9 @@ function Projects() {
   return (
     <div id="projects" className="projects--outer-container">
       <ProjectText />
+      <h2>Smarty</h2>
       <ProjectCard image={Smarty} skills={SmartySkills} link={"https://www.smarty.com"}/>
+      <h2>SmartRent</h2>
       <ProjectCard
         image={SmartRent}
         reverse={true}
@@ -19,6 +21,7 @@ function Projects() {
         skills={SmartRentSkills}
         link={"https://www.smartrent.com"}
       />
+      <h2>Blabu</h2>
       <ProjectCard image={Blabu} skills={BlabuSkills} link={"https://Blabugames.com"} />
     </div>
   );
