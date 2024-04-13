5636ce8567e769985ac6ab777ff1322af86c1325
Blake Trapnell <95154248+Ace-as-sin@users.noreply.github.com>
2024-04-12 19:05:19 -0600

    updating meta tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,34 +4,35 @@ import { Helmet } from "react-helmet";
 const Header = () => {
   return (
     <Helmet>
-      <title>Blake Trapnell: Web Developer extrodinaire</title>
+      <title>Blake Trapnell's Portfolio</title>
       <meta
         name="description"
-        content="Blake Trapnell portfolio, a one stop website for all my projects and information"
+        content="Check out Blake Trapnell's portfolio for his latest projects and information."
       />
-      <meta property="og:title" content="Blake Trapnell portfolio" />
+      {/* Add Open Graph and Twitter meta tags */}
+      <meta property="og:title" content="Blake Trapnell's Portfolio" />
       <meta
         property="og:description"
-        content="Blake Trapnell portfolio, a one stop website for all my projects and information"
+        content="Check out Blake Trapnell's portfolio for his latest projects and information."
       />
       <meta
         property="og:image"
         content="https://blaketrapnell.com/BlakeTrapnellWebsite.png"
       />
       <meta property="og:url" content="https://www.BlakeTrapnell.com" />
-      <meta name="twitter:title" content="Blake Trapnell portfolio" />
+      <meta name="twitter:title" content="Blake Trapnell's Portfolio" />
       <meta
         name="twitter:description"
-        content="Blake Trapnell portfolio, a one stop website for all my projects and information"
+        content="Check out Blake Trapnell's portfolio for his latest projects and information."
       />
       <meta
         name="twitter:image"
         content="https://blaketrapnell.com/BlakeTrapnellWebsite.png"
       />
-      <meta
-        name="twitter:card"
-        content="https://blaketrapnell.com/BlakeTrapnellWebsite.png"
-      />
+      <link rel="icon" href="%PUBLIC_URL%/BT.svg" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/BT.svg" />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
     </Helmet>
   );
 };
